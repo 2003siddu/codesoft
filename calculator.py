fa2b30bff8e8553405201cd7ad5de135db13256f
2003siddu <siddarthareddy981@gmail.com>
2024-06-14 08:58:30 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 def add(num1, num2):
     return num1 + num2
  
-# Function to subtract two numbers
+
 def subtract(num1, num2):
     return num1 - num2
  
-# Function to multiply two numbers
+
 def multiply(num1, num2):
     return num1 * num2
  
-# Function to divide two numbers
+
 def divide(num1, num2):
     return num1 / num2
  
@@ -20,7 +20,7 @@ print("Please select operation -\n" \
         "4. Divide\n")
  
  
-# Take input from the user
+
 select = int(input("Select operations form 1, 2, 3, 4 :"))
  
 number_1 = int(input("Enter first number: "))
